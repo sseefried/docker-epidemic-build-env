ba7980eae8d8b0aea80385e197f8c7e59b887dbc
Sean Seefried <sean.seefried@gmail.com>
2015-05-24 10:23:19 +1000

    Remove a comment

--- scripts/build-SDL2_mixer-mobile.sh
+++ scripts/build-SDL2_mixer-mobile.sh
@@ -6,8 +6,6 @@ source $THIS_DIR/set-env.sh
 
 cd SDL2_mixer-mobile
 
-
-#LDFLAGS="-Wl,--undefined=Java_org_libsdl_app_SDLActivity_nativeInit -ldl -lGLESv1_CM -lGLESv2 -llog -landroid" \
 ./configure --host=$HOST --prefix=$NDK_ADDON_PREFIX --disable-shared --enable-static \
   --enable-music-ogg --disable-music-midi-native
 
